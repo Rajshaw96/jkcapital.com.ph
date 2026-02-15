f25daa49a6e71184e6c61bc5315216d7a7123ada
Rajkumar Verma <rajkumarshaw624@gmail.com>
2026-02-16 01:21:31 +0530

    fixed In The Spotlight

--- src/app/shared/components/in-the-spotlight/in-the-spotlight.component.ts
+++ src/app/shared/components/in-the-spotlight/in-the-spotlight.component.ts
@@ -5,6 +5,7 @@ interface SpotlightItem {
   category: string;
   date: string;
   title: string;
+  link?: string;
 }
 
 @Component({
@@ -16,22 +17,25 @@ export class InTheSpotlightComponent {
 
   items: SpotlightItem[] = [
     {
-      image: 'assets/img/spotlight-1.png',
-      category: 'Business Loan',
-      date: '15 OCT, 2025',
-      title: 'Gorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.'
+      image: 'assets/img/blogs/blog-img-1.png',
+      category: 'Business Breakthrough',
+      date: 'January 19, 2026',
+      title: 'Excelucent Equipment Corporation Secures Growth Funding from JK Capital Finance',
+      link: 'https://jkcapital.com.ph/our-blog/excelucent-equipment-corporation-secures-growth-funding-from-jk-capital-finance/'
     },
     {
-      image: 'assets/img/spotlight-2.png',
-      category: 'Business Loan',
-      date: '17 OCT, 2025',
-      title: 'Gorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.'
+      image: 'assets/img/blogs/blog-img-2.jpg',
+      category: 'Business Loans',
+      date: 'December 19, 2025',
+      title: 'The Challenge: traditional bank loans take months. JK Capital solution: SME financing in 5-7 days',
+      link: 'https://jkcapital.com.ph/our-blog/the-challenge-traditional-bank-loans-take-months-jk-capital-solution-sme-financing-in-5-7-days/'
     },
     {
-      image: 'assets/img/spotlight-3.png',
-      category: 'Small Business',
-      date: '19 OCT, 2025',
-      title: 'Gorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.'
+      image: 'assets/img/blogs/blog-img-3.jpg',
+      category: 'Business Loans',
+      date: 'November 19, 2025',
+      title: 'Beyond Capital: Powering SME Growth in 2025 with JK Capital Financing Solutions',
+      link: 'https://jkcapital.com.ph/our-blog/beyond-capital-powering-sme-growth-in-2025-with-jk-capital-financing-solutions/'
     }
   ];
 }
