140b3d0dc656eb002dc3e2ea10b4ac9ed959b587
Rajkumar Verma <rajkumarshaw624@gmail.com>
2026-02-16 02:31:09 +0530

    Added in all Industries page

--- src/app/shared/components/industries/agriculture-sec1/agriculture-sec1.component.ts
+++ src/app/shared/components/industries/agriculture-sec1/agriculture-sec1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-agriculture-sec1',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./agriculture-sec1.component.css']
 })
 export class AgricultureSec1Component {
+
+  @Input() title!: string;
+  @Input() subtitle!: string;
+
+  @Input() card1Image!: string;
+  @Input() card1Title!: string;
+  @Input() card1Description!: string;
+
+  @Input() card2Image!: string;
+  @Input() card2Title!: string;
+  @Input() card2Description!: string;
 
 }
