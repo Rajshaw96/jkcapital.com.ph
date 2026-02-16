51d4cba30b82f5eeb6a99eb746d33ffa85cc42e9
Rajkumar Verma <rajkumarshaw624@gmail.com>
2026-02-17 01:31:40 +0530

    All Industries page image updated

--- src/app/shared/components/in-the-spotlight/in-the-spotlight.component.ts
+++ src/app/shared/components/in-the-spotlight/in-the-spotlight.component.ts
@@ -17,21 +17,21 @@ export class InTheSpotlightComponent {
 
   items: SpotlightItem[] = [
     {
-      image: 'assets/img/blogs/blog-img-1.png',
+      image: 'assets/img/blogs/Excelucent.jpg',
       category: 'Business Breakthrough',
       date: 'January 19, 2026',
       title: 'Excelucent Equipment Corporation Secures Growth Funding from JK Capital Finance',
       link: 'https://jkcapital.com.ph/our-blog/excelucent-equipment-corporation-secures-growth-funding-from-jk-capital-finance/'
     },
     {
-      image: 'assets/img/blogs/blog-img-2.jpg',
+      image: 'assets/img/blogs/The Challenge.JPG',
       category: 'Business Loans',
       date: 'December 19, 2025',
       title: 'The Challenge: traditional bank loans take months. JK Capital solution: SME financing in 5-7 days',
       link: 'https://jkcapital.com.ph/our-blog/the-challenge-traditional-bank-loans-take-months-jk-capital-solution-sme-financing-in-5-7-days/'
     },
     {
-      image: 'assets/img/blogs/blog-img-3.jpg',
+      image: 'assets/img/blogs/Beyond Capital.jpg',
       category: 'Business Loans',
       date: 'November 19, 2025',
       title: 'Beyond Capital: Powering SME Growth in 2025 with JK Capital Financing Solutions',
