a237423e06d370526bbe86fd922d5dac30b3138c
Rajkumar Shaw <97051975+Rajshaw96@users.noreply.github.com>
2026-03-04 12:12:41 +0530

    update

--- src/app/website/pages/knowledge-hub/news-section/news-section.component.ts
+++ src/app/website/pages/knowledge-hub/news-section/news-section.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class NewsSectionComponent {
 
+  batchSize = 4;
+
   categories = [
     'Our Story & News',
     'Business Loans',
@@ -15,48 +17,62 @@ export class NewsSectionComponent {
     'Loan Consultant'
   ];
 
-  activeCategory = this.categories[0];
-
-  batchSize = 4;
-  startIndex = 0;
+  startIndex: any = {};
 
   newsData: any = {
+
     'Our Story & News': [
-      { image: 'assets/img1.jpg', title: 'News Title 1' },
-      { image: 'assets/img2.jpg', title: 'News Title 2' },
-      { image: 'assets/img3.jpg', title: 'News Title 3' },
-      { image: 'assets/img4.jpg', title: 'News Title 4' },
-      { image: 'assets/img5.jpg', title: 'News Title 5' },
-      { image: 'assets/img6.jpg', title: 'News Title 6' }
+      { image:'assets/img1.jpg', title:'News Title 1'},
+      { image:'assets/img2.jpg', title:'News Title 2'},
+      { image:'assets/img3.jpg', title:'News Title 3'},
+      { image:'assets/img4.jpg', title:'News Title 4'},
+      { image:'assets/img5.jpg', title:'News Title 5'}
     ],
 
     'Business Loans': [
-      { image: 'assets/img1.jpg', title: 'Loan Article 1' },
-      { image: 'assets/img2.jpg', title: 'Loan Article 2' },
-      { image: 'assets/img3.jpg', title: 'Loan Article 3' },
-      { image: 'assets/img4.jpg', title: 'Loan Article 4' },
-      { image: 'assets/img5.jpg', title: 'Loan Article 5' }
+      { image:'assets/img1.jpg', title:'Loan Article 1'},
+      { image:'assets/img2.jpg', title:'Loan Article 2'},
+      { image:'assets/img3.jpg', title:'Loan Article 3'},
+      { image:'assets/img4.jpg', title:'Loan Article 4'},
+      { image:'assets/img5.jpg', title:'Loan Article 5'}
+    ],
+
+    'Finance 101': [
+      { image:'assets/img1.jpg', title:'Finance Article 1'},
+      { image:'assets/img2.jpg', title:'Finance Article 2'},
+      { image:'assets/img3.jpg', title:'Finance Article 3'},
+      { image:'assets/img4.jpg', title:'Finance Article 4'},
+      { image:'assets/img5.jpg', title:'Finance Article 5'}
     ]
+
   };
 
-  get visibleItems() {
-    const items = this.newsData[this.activeCategory] || [];
-    return items.slice(this.startIndex, this.startIndex + this.batchSize);
-  }
 
-  selectCategory(category: string) {
-    this.activeCategory = category;
-    this.startIndex = 0;
-  }
+  getVisibleItems(category:string){
 
-  nextBatch() {
-    const items = this.newsData[this.activeCategory] || [];
-
-    if (this.startIndex + this.batchSize < items.length) {
-      this.startIndex += this.batchSize;
-    } else {
-      this.startIndex = 0;
+    if(!this.startIndex[category]){
+      this.startIndex[category] = 0;
     }
+
+    const items = this.newsData[category] || [];
+
+    return items.slice(
+      this.startIndex[category],
+      this.startIndex[category] + this.batchSize
+    );
+  }
+
+
+  nextBatch(category:string){
+
+    const items = this.newsData[category] || [];
+
+    if(this.startIndex[category] + this.batchSize < items.length){
+      this.startIndex[category] += this.batchSize;
+    }else{
+      this.startIndex[category] = 0;
+    }
+
   }
 
 }
