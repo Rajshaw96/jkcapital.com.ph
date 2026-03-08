e0f591a38bb3d70f3d050db3cb22449f54cd7a9b
Rajkumar Verma <rajkumarshaw624@gmail.com>
2026-03-08 13:28:05 +0530

    fix image loading

--- src/app/website/pages/knowledge-hub/blog-details/blog-details.component.ts
+++ src/app/website/pages/knowledge-hub/blog-details/blog-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from '../services/blog.service';
 import { Blog } from '../models/blog.model';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser'; // Import these
 
 @Component({
   selector: 'app-blog-details',
@@ -9,24 +10,43 @@ import { Blog } from '../models/blog.model';
   styleUrls: ['./blog-details.component.css']
 })
 export class BlogDetailsComponent implements OnInit {
-
   blog: Blog | undefined;
+  safeContent: SafeHtml | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private blogService: BlogService
+    private blogService: BlogService,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit(): void {
-
     const slug = this.route.snapshot.paramMap.get('slug');
+    if (slug) {
+      this.blogService.getBlogs().subscribe((blogs: Blog[]) => {
+        const foundBlog = blogs.find(b => b.slug === slug);
 
-    this.blogService.getBlogs().subscribe((blogs: Blog[]) => {
-
-      this.blog = blogs.find(blog => blog.slug === slug);
-
-    });
-
+        if (foundBlog) {
+          this.blog = foundBlog;
+          if (this.blog.content) {
+            this.safeContent = this.sanitizer.bypassSecurityTrustHtml(this.blog.content);
+            setTimeout(() => {
+              this.fixWordPressImages();
+            }, 100);
+          }
+        }
+      });
+    }
   }
 
+  fixWordPressImages() {
+    const images = document.querySelectorAll('.blog-content img');
+    images.forEach((img: any) => {
+      img.setAttribute('referrerpolicy', 'no-referrer');
+      img.onerror = () => {
+        if (img.srcset) {
+          img.removeAttribute('srcset');
+        }
+      };
+    });
+  }
 }
